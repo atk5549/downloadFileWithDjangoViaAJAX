e2d6a6535e4706fdc636c233fd219477a066bf0b
Roman Kovalchuk <94655966+atk5549@users.noreply.github.com>
2023-09-05 18:06:47 +1000

    Update send_data_to_djangoBackend_working.js

--- clone/static/js/send_data_to_djangoBackend_working.js
+++ clone/static/js/send_data_to_djangoBackend_working.js
@@ -2,6 +2,23 @@ const myForm = document.getElementById('comment-form');
 
 myForm.addEventListener('click', () => {
 
+  // Генерирую uuid4 для названия файла
+  function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+  }
+
   function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -17,18 +34,23 @@ myForm.addEventListener('click', () => {
     return cookieValue;
   }
   var csrftoken = getCookie('csrftoken');
-
-
-
+  var mydata = generateUUID("Kovalchuk Roman")
   $.ajax({
+    url: '/download/',
     method: "POST",
-    headers: {'X-CSRFToken': csrftoken},
-    url: this.action,
-    data: 'Mytext',
-    dataType: "text",
+     headers: {
+      'Accept': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRFToken': csrftoken
+    },
+    data: {"name": mydata},
+    dataType: "xml",
     cache: false,
     success: function() {
-      console.log('WORK WORK @@@')
+      console.log('GOOD!!! SUCCESS QUERY!!!')
+    },
+    error: function() {
+      console.log(`NO GOOD!!! ${Error}`)
     }
   })
 })
